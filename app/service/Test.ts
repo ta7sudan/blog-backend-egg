7767e67c86f0baee781ee0cfb75518d98528c971
ta7sudan <ta7sudan@gmail.com>
2019-03-02 00:36:10 +0800

    update

--- app/service/Test.ts
+++ app/service/Test.ts
@@ -5,11 +5,11 @@ import { Service } from 'egg';
  */
 export default class Test extends Service {
 
-  /**
-   * sayHi to you
-   * @param name - your name
-   */
-  public async sayHi(name: string) {
-    return `hi, ${name}`;
-  }
+	/**
+	 * sayHi to you
+	 * @param name - your name
+	 */
+	public async sayHi(name: string) {
+		return `hi, ${name}`;
+	}
 }
